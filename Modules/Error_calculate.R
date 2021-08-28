9c9d2cfbef91731ad1ca0c4385a2fffe074cb525
Republic <ivan@npwc.us>
2021-08-28 16:03:44 +1200

    test

--- Modules/Error_calculate.R
+++ Modules/Error_calculate.R
@@ -270,6 +270,7 @@ Report<<-list(date1=basename(labelInput),
 			 
  write.csv(Compare1,save_pth_check_diff); print(Report)          
 }
+test 
 #######################################################################################################
 #########################################################################################################	
 #if (Species %in% c("WLRS")  & file.exists(ModelPoligonPTH) & file.exists(ObserverPointPTH)) {
